f43f11218efce33a30add2bad0fabfb561654cbe
paskuvan <majopaskuvan@gmail.com>
2019-01-09 18:54:17 -0300

    first commit

---
+++ ex01.rb
@@ -0,0 +1,22 @@
+class Alumno
+  attr_reader :nombre, :nota1, :nota2, :nota3, :nota4		
+  def initialize(nombre, nota1, nota2, nota3, nota4)
+    @nombre = nombre
+    @nota1 = nota1
+    @nota2 = nota2
+    @nota3 = nota3
+    @nota4 = nota4
+  end
+
+def self.read_file(filename = 'notas.txt')
+alumnos = []
+data = []
+File.open(filename, 'r') { |file| data = file.readlines  }
+data.each do |alumno|
+  alumnos << Alumno.new(*alumno.split(', '))
+end
+print alumnos
+end
+end 
+Alumno.read_file('notas.txt')
+
